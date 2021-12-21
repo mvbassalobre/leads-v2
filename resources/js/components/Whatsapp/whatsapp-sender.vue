da9afaa80ce2b9dd25b43cdffbf511e04f4a0bc3
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2021-12-21 13:55:08 -0300

    implementa eventos

--- resources/js/components/Whatsapp/whatsapp-sender.vue
+++ resources/js/components/Whatsapp/whatsapp-sender.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        <button @click="test">Testar</button>
+    <div class="d-flex flex-column">
+        <div v-if="!action || action == 'logged'">
+            <button @click="startOrClose">
+                {{ action ? "Fechar Conexão" : "Abrir Conexão" }}
+            </button>
+        </div>
+        <template v-if="action == 'loading'"> Carregando ... </template>
+        <template v-if="action == 'show_qr_code'">
+            <div>
+                <img class="mt-4" :src="qr_code.base64Qrimg" />
+            </div>
+        </template>
+        <template v-if="action == 'logged'">
+            <h1>Usuário Logado</h1>
+            <input v-model="message.phone_number" placeholder="Número do telefone" />
+            <textarea v-model="message.body" />
+            <button @click="sendMessage">Enviar</button>
+        </template>
     </div>
 </template>
 <script>
@@ -10,6 +26,20 @@ export default {
     data() {
         return {
             socket: null,
+            action: null,
+            qr_code: {},
+            // token: null,
+            token: {
+                WABrowserId: '"Zf4PxDjUuhJJ6Zlq72+exg=="',
+                WASecretBundle:
+                    '{"key":"+xYKIvufXJZdpTP75eTLKbBcFfNjjvcNudv5pi8CLYc=","encKey":"rEWvgBaYfkmz0ef5DKYdvQxqvXUD/LQyZHfORYTcwt8=","macKey":"+xYKIvufXJZdpTP75eTLKbBcFfNjjvcNudv5pi8CLYc="}',
+                WAToken1: '"SbUJt77PRnilZo4gOuUukJtfvCqmQAGmS9QMIO0CuaQ="',
+                WAToken2: '"1@RWaRytdSLRGbr2rMCGp2odPL8RiaQEfSGbLT7RoRSVfTs2r0BNchZrjs/LLpi08mABTXijuIfnbn1g=="',
+            },
+            message: {
+                phone_number: null,
+                body: null,
+            },
         };
     },
     created() {
@@ -22,22 +52,55 @@ export default {
                 reconnectionDelay: 500,
                 reconnectionAttempts: 10,
             });
+
             this.socket.on("connect", () => {
                 console.log(`connect ${this.socket.id}`);
             });
 
             this.socket.on("disconnect", () => {
-                console.log("disconnect");
+                this.action = null;
+                console.log("disconnect ...");
+            });
+        },
+        startOrClose() {
+            if (this.action) {
+                this.action = null;
+                return this.socket.emit("close-connection");
+            }
+            this.action = "loading";
+            this.socket.emit("start-engine", { token: this.token });
+
+            this.socket.on("qr-generated", (data) => {
+                this.qr_code = data;
+                this.action = "show_qr_code";
             });
 
-            this.socket.on("test-socket", () => {
-                console.log("teste");
+            this.socket.on("session-updated", (data) => {
+                if (data.statusSession == "qrReadSuccess") {
+                    this.action = "loading";
+                }
             });
-            console.log("init ...", this.socket);
+
+            this.socket.on("token-generated", (data) => {
+                this.action = "logged";
+                this.token = data.token;
+            });
         },
-        test() {
-            this.socket.emit("test-connected-user", (data) => {
-                console.log(data);
+        sendMessage() {
+            this.socket.emit("send-message", {
+                phone_number: this.message.phone_number,
+                body: this.message.body,
+            });
+
+            this.socket.on("sent-message", () => {
+                alert("Mensagem enviada !!");
+                Object.keys(this.message).map((key) => {
+                    this.message[key] = null;
+                });
+            });
+
+            this.socket.on("message-failed", (er) => {
+                console.log(er);
             });
         },
     },
